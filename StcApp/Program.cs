4f4a551397959163d2a094d7eb90e577b296db31
Shailesh Bagade <shailesh.bagade@logstarerp.com>
2018-02-02 06:21:11 +0000

    hello world 8

--- StcApp/Program.cs
+++ StcApp/Program.cs
@@ -10,7 +10,7 @@ namespace StcApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World 8");
         }
     }
 }
